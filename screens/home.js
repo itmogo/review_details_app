b14dfcda5354c3a2667aa63a777f0d44c1266e19
edward frempong <eddysoftwarengineer@gmail.com>
2021-12-14 18:34:24 +0000

    created link to navigate from home to reviewdetails and back

--- screens/home.js
+++ screens/home.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function Home(){
+export default function Home({navigation}){
+
+    //onpress function on button when pressed
+     const pressHandler = () => {
+         navigation.navigate('ReviewDetails');
+        //new navigation code push up element in a stack
+        //navigation.push('ReviewDetails');
+    }
+
     return(
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Screen </Text>
-            <Text style={globalStyles.text} >He has done it again </Text>
+           
+           <Button title='go to review page' onPress={pressHandler}/>
         </View>
-    )
+    );
 }
 
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 import { globalStyles } from '../styles/global';
 
 
-export default function ReviewDetails(){
+export default function ReviewDetails({navigation}){
+
+    const pressHandler = () => {
+        navigation.goBack();
+    }
+
     return(
         <View style={globalStyles.container}>
-            <Text> Review Details Screen</Text>
+            <Text style={globalStyles.titleText}> Review Details Screen</Text>
+            <Button  title='back to home page' onPress={pressHandler} />
         </View>
         )
-    }
+    } 
 const styles = StyleSheet.create({
     container:{
         padding: 24,
