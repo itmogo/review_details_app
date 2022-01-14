17d7a03014e7f61b23f438f6b70d0593945bcaf9
edward frempong <eddysoftwarengineer@gmail.com>
2022-01-14 14:24:32 +0000

    completed custome card

--- screens/home.js
+++ screens/home.js
@@ -1,37 +1,123 @@
-import React, {useState} from 'react';
-import {StyleSheet, View, Text, FlatList, TouchableOpacity} from 'react-native';
-import { globalStyles } from '../styles/global';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import Card from "../shared/card";
+import { globalStyles } from "../styles/global";
 
-export default function Home({navigation}){
-    //passing data between screens using usestate 
-    const [reviews, setReviews] = useState([
- {number: 1, linkedln:'Adam Levin', title: 'Chairman & Founder', brand: 'CyberScout', twitter:'@Adam_K_Levin', location: 'Scottsdale, Ariz.', key: '1' },
- {number: 2, linkedln:'Adam Meyers', title: 'SVP Intelligence', brand: 'Crowdstrike', twitter:'@Adam_Cyber', location: 'Washington, D.C.', key: '2' },
- {number: 3, linkedln:'Alissa (Dr Jay) Abdullah, PhD', title: 'SVP & Deputy CSO', brand: 'Mastercard', twitter:'@dralissajay', location: 'Waldorf, Md.', key: '4' },
- {number: 5, linkedln:'Ann Johnson', title: 'CVP - Security, Compliance & Identity', brand: 'Microsoft', twitter:'@ajohnsocyber', location: 'Seattle, Wash.', key: '5' },
- {number: 6, linkedln:'Bob Carver', title: 'Principal Cybersecurity Threat Intelligence & Analytics', brand: 'Verizon', twitter:'@cybersecboardrm', location: 'Dallas, Texas', key: '6' },
- {number: 7, linkedln:'Brett Arsenault', title: 'CVP & CISO', brand: 'Microsoft', twitter:'@Microsoft', location: 'Bellevue, Wash.', key: '7' },
- {number: 8, linkedln:'Brian Honan', title: 'Technical Advisory Board Member', brand: 'Circit', twitter:'@BrianHonan', location: 'Ireland', key: '8' },
- {number: 9, linkedln:'Brian Krebs', title: 'Reporter & Publisher', brand: 'KrebsOnSecurity.com', twitter:'@briankrebs', location: 'Arlington, Va.', key: '9' },
- {number: 10, linkedln:'Bruce Schneier', title:'Security Guru', brand: 'Best-Selling Author', twitter:'@schneierblog', location: 'Boston, Mass.', key: '10' },
- {number: 11, linkedln:'Chuck Brooks', title: 'Adjunct Professor', brand: 'Georgetown University', twitter:'@ChuckDBrooks', location: 'Baltimore, Md.', key: '11' },
+export default function Home({ navigation }) {
+  //passing data between screens using usestate
+  const [reviews, setReviews] = useState([
+    {
+      number: 1,
+      linkedln: "Adam Levin",
+      title: "Chairman & Founder",
+      brand: "CyberScout",
+      twitter: "@Adam_K_Levin",
+      location: "Scottsdale, Ariz.",
+      key: "1",
+    },
+    {
+      number: 2,
+      linkedln: "Adam Meyers",
+      title: "SVP Intelligence",
+      brand: "Crowdstrike",
+      twitter: "@Adam_Cyber",
+      location: "Washington, D.C.",
+      key: "2",
+    },
+    {
+      number: 3,
+      linkedln: "Alissa (Dr Jay) Abdullah, PhD",
+      title: "SVP & Deputy CSO",
+      brand: "Mastercard",
+      twitter: "@dralissajay",
+      location: "Waldorf, Md.",
+      key: "4",
+    },
+    {
+      number: 5,
+      linkedln: "Ann Johnson",
+      title: "CVP - Security, Compliance & Identity",
+      brand: "Microsoft",
+      twitter: "@ajohnsocyber",
+      location: "Seattle, Wash.",
+      key: "5",
+    },
+    {
+      number: 6,
+      linkedln: "Bob Carver",
+      title: "Principal Cybersecurity Threat Intelligence & Analytics",
+      brand: "Verizon",
+      twitter: "@cybersecboardrm",
+      location: "Dallas, Texas",
+      key: "6",
+    },
+    {
+      number: 7,
+      linkedln: "Brett Arsenault",
+      title: "CVP & CISO",
+      brand: "Microsoft",
+      twitter: "@Microsoft",
+      location: "Bellevue, Wash.",
+      key: "7",
+    },
+    {
+      number: 8,
+      linkedln: "Brian Honan",
+      title: "Technical Advisory Board Member",
+      brand: "Circit",
+      twitter: "@BrianHonan",
+      location: "Ireland",
+      key: "8",
+    },
+    {
+      number: 9,
+      linkedln: "Brian Krebs",
+      title: "Reporter & Publisher",
+      brand: "KrebsOnSecurity.com",
+      twitter: "@briankrebs",
+      location: "Arlington, Va.",
+      key: "9",
+    },
+    {
+      number: 10,
+      linkedln: "Bruce Schneier",
+      title: "Security Guru",
+      brand: "Best-Selling Author",
+      twitter: "@schneierblog",
+      location: "Boston, Mass.",
+      key: "10",
+    },
+    {
+      number: 11,
+      linkedln: "Chuck Brooks",
+      title: "Adjunct Professor",
+      brand: "Georgetown University",
+      twitter: "@ChuckDBrooks",
+      location: "Baltimore, Md.",
+      key: "11",
+    },
+  ]);
 
- 
-       
-    ]);   
-
-    return(
-        <View style={globalStyles.container}>
-        <FlatList 
-            data={reviews}
-            renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
-                    <Text style={globalStyles.titleText}>{item.linkedln}</Text>                
-                </TouchableOpacity>
-            )}       
-        />         
-                     
-        </View>
-    )
+  return (
+    <View style={globalStyles.container}>
+      <FlatList
+        data={reviews}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ReviewDetails", item)}
+          >
+            <Card>{/*added card component around text*/}
+              <Text style={globalStyles.titleText}>{item.linkedln}</Text>
+            </Card>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
 }
-
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
 import { globalStyles } from '../styles/global';
+import Card from '../shared/card';
+
 
 
 export default function ReviewDetails({navigation}){ 
 
     return(
         <View style={globalStyles.container}>
+            <Card>
             <Text style={globalStyles.text}> Number: {navigation.getParam ('number')} </Text>
             <Text style={globalStyles.text}> Linkedln: {navigation.getParam('linkedln')} </Text>
             <Text style={globalStyles.text}> Title: {navigation.getParam('title')} </Text>
             <Text style={globalStyles.text}> Brand: {navigation.getParam('brand')} </Text>
             <Text style={globalStyles.text}> Twitter: {navigation.getParam('twitter')} </Text>
             <Text style={globalStyles.text}> Location: {navigation.getParam('location')} </Text> 
+            </Card>
         </View>
         )
     } 
